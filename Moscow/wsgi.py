4f90015411ebfa6517cce4a67b9f5ace603eef53
Stephen XIONG <jakrinchose@gmail.com>
2018-01-15 13:59:56 +0800

    change project name from Virgo to Moscow

--- Moscow/wsgi.py
+++ Moscow/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Virgo project.
+WSGI config for Moscow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Virgo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Moscow.settings")
 
 application = get_wsgi_application()
