f3731b182aeac3a6d428b295ee39b3d51865b985
Stephen XIONG <jakrinchose@gmail.com>
2018-01-23 13:54:56 +0800

    udpate home api

--- api/views/home.py
+++ api/views/home.py
@@ -17,14 +17,32 @@ class Home(View):
     @check_session
     def get(self, request):
         res = {
-            'product_data_list': [
+            "banner_img_list": [
                 {
-                    'id': 1,
-                    'name': '测试商品',
-                    'price_yh': 20.00, 
-                    'price': 18.88, 
-                    'is_show': 1,
-                    'shiyong': 100
+                    "url": "../goods/detail?goodsId=1000",
+                    "img": "http://xiaobaidian-img-001-1255633922.picgz.myqcloud.com/banner1.jpeg",
+                },
+                {
+                    "url": "../goods/detail?goodsId=1000",
+                    "img": "http://xiaobaidian-img-001-1255633922.picgz.myqcloud.com/banner2.jpeg"
+                }
+            ],
+            "goods_list": [
+                {
+                    "goods_id": 1000,
+                    "goods_name": "爆款毛衣",
+                    "goods_price": 99.99,
+                    "market_price": 129.99,
+                    "goods_img": "http://xiaobaidian-img-001-1255633922.picgz.myqcloud.com/test_goods_1.jpeg",
+                    "url": "../goods/detail?goodsId=1000",
+                },
+                {
+                    "goods_id": 1000,
+                    "goods_name": "爆款毛衣",
+                    "goods_price": 99.99,
+                    "market_price": 129.99,
+                    "goods_img": "http://xiaobaidian-img-001-1255633922.picgz.myqcloud.com/test_goods_1.jpeg",
+                    "url": "../goods/detail?goodsId=1000",
                 }
             ]
         }
