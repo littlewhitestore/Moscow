d4efc9d4c0599f6850b4c785ba0e610a3e3e94a4
Stephen XIONG <jakrinchose@gmail.com>
2018-01-29 01:29:30 +0800

    update settings

--- Moscow/settings.py
+++ Moscow/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 #    'django.contrib.admin',
 #    'django.contrib.auth',
     'django.contrib.contenttypes',
-#    'django.contrib.sessions',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-#    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     #'django.contrib.auth.middleware.AuthenticationMiddleware',
