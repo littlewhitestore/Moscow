95cf3a9813d961d7e351913f2b218a44d2d40055
Stephen XIONG <jakrinchose@gmail.com>
2018-02-25 15:01:23 +0800

    add ImageStorage class

--- common/utils/__init__.py
+++ common/utils/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+import hashlib
 import pytz
 import time
 
@@ -17,3 +18,29 @@ def epoch(t):
     if not hasattr(t, 'tzinfo'):
         return
     return int(time.mktime(_append_tz(t).timetuple()))
+
+
+class ImageStorage(object):
+    BUCKET_SIZE = 16
+    QCLOUD_CONFIG = {
+        'secret_id': 'AKIDjOTO9qnlxyoBe2aJB2vmTzKVYVL5m6NT',
+        'secret_key': '65t0xlYEnCQn2771nR3Ys18A2oT3Yy31',
+        'region': 'ap-beijing',
+    }
+    
+    @staticmethod
+    def generate_stream_key(stream):
+        handler = hashlib.md5().update(stream)
+        key = handler.hexdigest()
+        return key
+    
+    @staticmethod
+    def get_bucket_by_key(key):
+        bucket = "img-prod-{:0>3d}-1255633922".format(hash(key) % ImageStorage.BUCKET_SIZE + 1)
+        #change to the first char of md5 hash
+        return bucket
+
+    @staticmethod
+    def get_url_by_key(key):
+        pass
+        
